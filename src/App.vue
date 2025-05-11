66ca59117d35f38cd619bcad405ea5a1fd53d3c5
hinatanoyume13 <hinatanoyume13@gmail.com>
2025-05-12 02:48:14 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import StarCanvas from './components/StarCanvas.vue'
+</script>
+
+<template>
+ 
+ 
+    <h1>星を表示するテスト</h1>
+ 
+ 
+  <div>
+    <StarCanvas />
+  </div>
+
+
+</template>
+
+<style scoped>
+/* 必要ならスタイルをここに追加 */
+</style>
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: './', // ← これが重要（相対パスで読み込む）
+})
